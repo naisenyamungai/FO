a35829761fefdfa46bc71e347d6a6e4582fdb4b3
naisenyamungai <naisenyamungai@gmail.com>
2019-05-30 20:56:02 +0300

    create siteTest.java

---
+++ src/main/java/Site.java
@@ -0,0 +1,13 @@
+import org.sql2o.*;
+
+public class Site{
+    private String name;
+    private int personId;
+    private int id;
+
+    public Site(String name, int personId) {
+        this.name = name;
+        this.personId = personId;
+
+   }
+}

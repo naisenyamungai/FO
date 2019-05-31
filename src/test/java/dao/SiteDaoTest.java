6982b2fc4c1470182887e35270bc16020ce86eda
naisenyamungai <naisenyamungai@gmail.com>
2019-05-31 09:45:39 +0300

    create files that will directly interact with the database

---
+++ src/test/java/dao/SiteDaoTest.java
@@ -0,0 +1,18 @@
+package dao;
+import models.Site;
+import org.junit.After;
+import org.junit.Before;
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SiteDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
